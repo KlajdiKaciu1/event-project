90c63fb882bd87caf9b2b9724cadfeb5096137ca
Klajdi <klajdikaciu@gmail.com>
2024-06-24 19:02:36 +0200

    Added mongoosedb connection and fixed bugs

--- backend/index.js
+++ backend/index.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const cors = require('cors');
+const mongoose = require('mongoose');
+require('dotenv').config();
 const app = express();
 
 app.use(express.json());
@@ -8,10 +10,10 @@ app.use(cors({
   origin: 'http://localhost:5173',
 }));
 
+mongoose.connect(process.env.MONGO_URL);
 app.get('/test', (req, res) => {
     res.json('test ok');
   });
-
 app.post('/register', (req, res) => {
   const { name, email, password } = req.body;
   res.json({ name, email, password });
