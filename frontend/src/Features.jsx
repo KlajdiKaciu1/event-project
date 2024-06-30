59308acdbd1267ad5f898f8a24cda2240b8ea458
Klajdi <klajdikaciu@gmail.com>
2024-06-30 18:56:37 +0200

    Added changes to event form

---
+++ frontend/src/Features.jsx
@@ -0,0 +1,36 @@
+export default function Features({selected,onChange}){
+    return(
+        <>
+        <div className="grid mt-2 gap-2 grid-cols-2 md:grid-cols-3 lg:grid-cols-6">
+                          <label className="border p-4 flex rounded-2xl gap-2 items-center cursor-pointer">
+                            <input type="checkbox"/>
+                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
+                                <path strokeLinecap="round" strokeLinejoin="round" d="m20.893 13.393-1.135-1.135a2.252 2.252 0 0 1-.421-.585l-1.08-2.16a.414.414 0 0 0-.663-.107.827.827 0 0 1-.812.21l-1.273-.363a.89.89 0 0 0-.738 1.595l.587.39c.59.395.674 1.23.172 1.732l-.2.2c-.212.212-.33.498-.33.796v.41c0 .409-.11.809-.32 1.158l-1.315 2.191a2.11 2.11 0 0 1-1.81 1.025 1.055 1.055 0 0 1-1.055-1.055v-1.172c0-.92-.56-1.747-1.414-2.089l-.655-.261a2.25 2.25 0 0 1-1.383-2.46l.007-.042a2.25 2.25 0 0 1 .29-.787l.09-.15a2.25 2.25 0 0 1 2.37-1.048l1.178.236a1.125 1.125 0 0 0 1.302-.795l.208-.73a1.125 1.125 0 0 0-.578-1.315l-.665-.332-.091.091a2.25 2.25 0 0 1-1.591.659h-.18c-.249 0-.487.1-.662.274a.931.931 0 0 1-1.458-1.137l1.411-2.353a2.25 2.25 0 0 0 .286-.76m11.928 9.869A9 9 0 0 0 8.965 3.525m11.928 9.868A9 9 0 1 1 8.965 3.525" />
+                            </svg>
+                            <span>Erasmus study-programmes</span>
+                          </label>
+                          <label className="border p-4 flex rounded-2xl gap-2 items-center cursor-pointer">
+                            <input type="checkbox"/>
+                             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
+                                <path strokeLinecap="round" strokeLinejoin="round" d="M16.5 18.75h-9m9 0a3 3 0 0 1 3 3h-15a3 3 0 0 1 3-3m9 0v-3.375c0-.621-.503-1.125-1.125-1.125h-.871M7.5 18.75v-3.375c0-.621.504-1.125 1.125-1.125h.872m5.007 0H9.497m5.007 0a7.454 7.454 0 0 1-.982-3.172M9.497 14.25a7.454 7.454 0 0 0 .981-3.172M5.25 4.236c-.982.143-1.954.317-2.916.52A6.003 6.003 0 0 0 7.73 9.728M5.25 4.236V4.5c0 2.108.966 3.99 2.48 5.228M5.25 4.236V2.721C7.456 2.41 9.71 2.25 12 2.25c2.291 0 4.545.16 6.75.47v1.516M7.73 9.728a6.726 6.726 0 0 0 2.748 1.35m8.272-6.842V4.5c0 2.108-.966 3.99-2.48 5.228m2.48-5.492a46.32 46.32 0 0 1 2.916.52 6.003 6.003 0 0 1-5.395 4.972m0 0a6.726 6.726 0 0 1-2.749 1.35m0 0a6.772 6.772 0 0 1-3.044 0" />
+                             </svg>
+                            <span>Hackathon</span>
+                          </label>
+                          <label className="border p-4 flex rounded-2xl gap-2 items-center cursor-pointer">
+                            <input type="checkbox"/>
+                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
+                                <path strokeLinecap="round" strokeLinejoin="round" d="M20.25 14.15v4.25c0 1.094-.787 2.036-1.872 2.18-2.087.277-4.216.42-6.378.42s-4.291-.143-6.378-.42c-1.085-.144-1.872-1.086-1.872-2.18v-4.25m16.5 0a2.18 2.18 0 0 0 .75-1.661V8.706c0-1.081-.768-2.015-1.837-2.175a48.114 48.114 0 0 0-3.413-.387m4.5 8.006c-.194.165-.42.295-.673.38A23.978 23.978 0 0 1 12 15.75c-2.648 0-5.195-.429-7.577-1.22a2.016 2.016 0 0 1-.673-.38m0 0A2.18 2.18 0 0 1 3 12.489V8.706c0-1.081.768-2.015 1.837-2.175a48.111 48.111 0 0 1 3.413-.387m7.5 0V5.25A2.25 2.25 0 0 0 13.5 3h-3a2.25 2.25 0 0 0-2.25 2.25v.894m7.5 0a48.667 48.667 0 0 0-7.5 0M12 12.75h.008v.008H12v-.008Z" />
+                            </svg>
+                            <span>Employment opportunities</span>
+                          </label>
+                          <label className="border p-4 flex rounded-2xl gap-2 items-center cursor-pointer">
+                            <input type="checkbox"/>
+                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
+                                <path strokeLinecap="round" strokeLinejoin="round" d="M12 6.042A8.967 8.967 0 0 0 6 3.75c-1.052 0-2.062.18-3 .512v14.25A8.987 8.987 0 0 1 6 18c2.305 0 4.408.867 6 2.292m0-14.25a8.966 8.966 0 0 1 6-2.292c1.052 0 2.062.18 3 .512v14.25A8.987 8.987 0 0 0 18 18a8.967 8.967 0 0 0-6 2.292m0-14.25v14.25" />
+                            </svg>
+                            <span>Paid scholarship</span>
+                          </label>
+                        </div>
+        </>
+    );
+}
--- frontend/src/pages/EventPage.jsx
+++ frontend/src/pages/EventPage.jsx
@@ -1,6 +1,32 @@
+import { useState } from "react";
 import {Link, useParams} from "react-router-dom";
+import Features from "../Features";
 export default function EventPage(){
     const {action} = useParams();
+    const [title,setTitle] = useState('');
+    const [address,setAddress] = useState('');
+    const [addedPhotos,setAddedPhotos] = useState([]);
+    const [photoLink,setPhotoLink] = useState('');
+    const [description,setDescription] = useState('');
+    const [features,setFeatures] = useState([]);
+    function inputHeader(text){
+        return(
+            <h2 className="text-2xl mt-4">{text}</h2>
+        );
+    }
+    function inputDescription(text){
+        return(
+            <p className="text-gray-500 text-sm">{text}</p>
+        );
+    }
+    function preInput(header,description){
+        return(
+            <>
+             {inputHeader(header)}
+             {inputDescription(description)}
+            </>
+        );
+    }
     return(
         <div>
             {action != 'new' && (
@@ -16,17 +42,36 @@ export default function EventPage(){
             {action === 'new' && (
                 <div>
                     <form>
-                        <h2 className="text-2xl mt-4">Title</h2>
-                        <p className="text-gray-500 text-sm">Title of the event</p>
-                        <input type="text" placeholder="title"/>
-                        <h2 className="text-2xl mt-4">Address</h2>
-                        <p className="text-gray-500 text-sm">Address to the event</p>
-                        <input type="text" placeholder="address"/>
+                        {preInput('Title','Title of the event')}
+                        <input type="text" value={title} 
+                        onChange={ev=>setTitle(ev.target.value)} 
+                        placeholder="title"/>
+                        {preInput('Address','Address to the event')}
+                        <input type="text" 
+                         value={address} onChange={ev=>setAddress(ev.target.value)}
+                          placeholder="address"/>
                         <h2 className="text-2xl mt-4">Photos</h2>
-                        <div className="mt-2 grid grid-cols-3 md:grid-cols-4 lg:grid-cols-6">
-                          <button className="border bg-transparent rounded-2xl p-8 text-2xl text-gray-600">+</button>
+                        <div className="flex gap-2">
+                            <input type="text"
+                             value={photoLink} onChange={ev=>setPhotoLink(ev.target.value)} 
+                             placeholder={"Add using link ...jpg"}/>
+                            <button className="bg-gray-200 px-4 rounded-2xl">Add&nbsp;photo</button>
                         </div>
-                        
+                        <div className="mt-2 grid grid-cols-3 md:grid-cols-4 lg:grid-cols-6">
+                          <button className=" flex gap-1 justify-center border bg-transparent rounded-2xl p-8 text-2xl text-gray-600">
+                             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-8">
+                              <path strokeLinecap="round" strokeLinejoin="round" d="M3 16.5v2.25A2.25 2.25 0 0 0 5.25 21h13.5A2.25 2.25 0 0 0 21 18.75V16.5m-13.5-9L12 3m0 0 4.5 4.5M12 3v13.5" />
+                             </svg>
+                            Upload
+                          </button>
+                        </div>
+                        {preInput('Description','Description of the event')} 
+                        <textarea value={description} onChange={ev=>setDescription(ev.target.value)}/>
+                        {preInput('Features','Select the features of the event')} 
+                        <Features selected={features} onChange={setFeatures}/>
+                        <div>
+                            <button className="primary my-4">Save</button>
+                        </div>
                     </form>
                 </div>
             )}
